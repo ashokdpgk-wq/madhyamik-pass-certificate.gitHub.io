ae630716679c5be4a27a130fa978b1f3af486527
ashokdpgk-wq <ashokdpgk@gmail.com>
2026-05-14 23:27:57 +0530

    Add files via upload

---
+++ 2026-April.js
@@ -0,0 +1,189 @@
+window.yearData = [
+  {
+    "appNo": "SCWB02042024/3095",
+    "applDate": "11/11/2024",
+    "name": "PIYASA ROY",
+    "father": "BIKASH ROY",
+    "certNo": "WB0204SC2026000804",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Female",
+    "address": "PURBA DURAMARI, P.O. : BANARHAT, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042026/1192",
+    "applDate": "22/2/2026",
+    "name": "ROLI SHAW",
+    "father": "MAHESH SHAW",
+    "certNo": "WB0204SC2026000805",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(NUNIYA)",
+    "gender": "Female",
+    "address": "PALASHBARI TEA GARDEN, P.O. : BANARHAT, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/2220",
+    "applDate": "31/1/2025",
+    "name": "ARYAN SHAW",
+    "father": "MAHESH SHAW",
+    "certNo": "WB0204SC2026000806",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(NUNIYA)",
+    "gender": "Male",
+    "address": "PALASHBARI T.G, P.O. : BANARHAT, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/6942",
+    "applDate": "13/9/2025",
+    "name": "PARIMAL ROY",
+    "father": "UPENDRA NATH ROY",
+    "certNo": "WB0204SC2026000807",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Male",
+    "address": "CHANADIPA, P.O. : PURBA DURAMARI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/6046",
+    "applDate": "28/8/2025",
+    "name": "SUBHAJEET ROY",
+    "father": "DHANANJOY ROY",
+    "certNo": "WB0204SC2026000809",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Male",
+    "address": "MADHYA SALBARI, P.O. : PURBA DURAMARI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/7498",
+    "applDate": "15/10/2025",
+    "name": "BIJAY BARMAN",
+    "father": "DWIJEN BARMAN",
+    "certNo": "WB0204SC2026000810",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Male",
+    "address": "UTTAR SHALBARI, P.O. : PURBA DURAMARI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/6661",
+    "applDate": "9/9/2025",
+    "name": "PARIPURNA MANDAL",
+    "father": "PRAHLAD MANDAL",
+    "certNo": "WB0204SC2026000811",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(NAMASUDRA)",
+    "gender": "Male",
+    "address": "PASCHIM DURAMARI, P.O. : PURBA DURAMARI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042024/1397",
+    "applDate": "5/8/2024",
+    "name": "MUNMUN ROY",
+    "father": "MANORANJAN ROY",
+    "certNo": "WB0204SC2026000812",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Female",
+    "address": "PURBA GAIRKATA, P.O. : GAIRKATA, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/6940",
+    "applDate": "13/9/2025",
+    "name": "RAHUL ROY",
+    "father": "PARIMAL ROY",
+    "certNo": "WB0204SC2026000813",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Male",
+    "address": "CHANADIPA, P.O. : PURBA DURAMARI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/6938",
+    "applDate": "13/9/2025",
+    "name": "DALIMA ROY",
+    "father": "BAIKUNTHA ROY",
+    "certNo": "WB0204SC2026000814",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Female",
+    "address": "CHANADIPA, P.O. : PURBA DURAMARI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/6967",
+    "applDate": "14/9/2025",
+    "name": "MALLIKA ROY",
+    "father": "LATE SUBODH ROY",
+    "certNo": "WB0204SC2026000815",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Female",
+    "address": "DAKSHIN SALBARI, P.O. : PURBA DURAMARI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042026/988",
+    "applDate": "13/2/2026",
+    "name": "MRIGANKA ROY",
+    "father": "KESHARI MOHAN ROY",
+    "certNo": "WB0204SC2026000816",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Male",
+    "address": "CHAMTIMUKHI, P.O. : LAXMIKANTAPARA, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042026/909",
+    "applDate": "10/2/2026",
+    "name": "TULSHI MANDAL SARKAR",
+    "father": "DILIP MANDAL",
+    "certNo": "WB0204SC2026000817",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(NAMASUDRA)",
+    "gender": "Female",
+    "address": "KHUDIRAM PALLY BANARHAT, P.O. : BANARHAT, P.S : BANARHAT, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042026/1378",
+    "applDate": "25/2/2026",
+    "name": "ANIL BISWAKARMA",
+    "father": "KUNDAN BISWAKARMA",
+    "certNo": "WB0204SC2026000818",
+    "issueDate": "2026-04-03",
+    "appliedFor": "SC(KAMI (NEPALI)",
+    "gender": "Male",
+    "address": "CHAMURCHI T.G, MANDIR LINE, P.O. : CHAMURCHI, P.S : BANARHAT, Jalpaiguri, WEST BENGAL )"
+  },
+  {
+    "appNo": "SCWB02042025/5279",
+    "applDate": "13/8/2025",
+    "name": "GOBINDA ROY",
+    "father": "HIREN ROY",
+    "certNo": "WB0204SC2026000819",
+    "issueDate": "2026-04-08",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Male",
+    "address": "KHALAIGRAM, P.O. : SALBARI, P.S : DHUPGURI, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/3145",
+    "applDate": "17/2/2025",
+    "name": "JATRA MOHAN ROY",
+    "father": "SATISH ROY",
+    "certNo": "WB0204SC2026000820",
+    "issueDate": "2026-04-08",
+    "appliedFor": "SC(RAJBANSHI)",
+    "gender": "Male",
+    "address": "UTTAR KHATTIMARI, P.O. : DAKSHIN KHATTIMARI, P.S : DHUPGURI, Jalpaiguri, WEST BENGAL"
+  },
+  {
+    "appNo": "SCWB02042025/5807",
+    "applDate": "25/8/2025",
+    "name": "BITTU ROY",
+    "father": "HARIDAS ROY",
+    "certNo": "WB0204SC2026000821",
+    "issueDate": "2026-04-08",
+    "appliedFor": "SC(NAMASUDRA)",
+    "gender": "Male",
+    "address": "MADHYA KHATTIMARI, P.O. : DAKSHINKHATTIMARI, P.S : DHUPGURI, Jalpaiguri, WEST BENGAL"
+  }
+];
